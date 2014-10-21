0ca4087d13d82a2e5c5845fbd25d33d663c79c42
ToothpickFactory <austin.d.lovell@gmail.com>
2014-10-21 10:36:15 -0600

    Script can now be placed any where on page. Also took care of scroll bar issue that added 18px to table.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
-$.each($('.fix-header'), function(){
-	var table = this;
-	$.each($('tbody tr:first', this).children(), function(index){
-		var tdWidth = $(this)[0].getBoundingClientRect().width;
-		var thWidth = $('thead th:nth-child('+(index+1)+')', table)[0].getBoundingClientRect().width;
-		if(thWidth > tdWidth){
-			$(this).width(thWidth);
-			$('tfoot th:nth-child('+(index+1)+')', table).width(thWidth);
-		}else{
-			$('thead th:nth-child('+(index+1)+')', table).width(tdWidth);
-			$('tfoot th:nth-child('+(index+1)+')', table).width(tdWidth);
-		}
+var renderTables = function(){
+	$.each($('.fx'), function(){
+		var table = this;
+		$.each($('tbody tr:first', this).children(), function(index){
+			var tdWidth = $(this)[0].getBoundingClientRect().width;
+			var thWidth = $('thead th:nth-child('+(index+1)+')', table)[0].getBoundingClientRect().width;
+			if(thWidth > tdWidth){
+				$(this).width(thWidth - 18);
+				$('tfoot th:nth-child('+(index+1)+')', table).width(thWidth - 18);
+			}else{
+				console.log(tdWidth)
+				$('thead th:nth-child('+(index+1)+')', table).width(tdWidth - 18);
+				$('tfoot th:nth-child('+(index+1)+')', table).width(tdWidth - 18);
+			}
+		});
 	});
-});
+}
+
+$(window).load(renderTables)
